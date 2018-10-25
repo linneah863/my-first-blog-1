bdf2c38f053f6f7f684420a4a5d2b3629afd4a77
Konstantin Stepanov <me@kstep.me>
2018-10-25 18:00:04 +0300

    switch to class-based views

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
-    url(r'^post/new/$', views.post_new, name='post_new'),
-    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
+    url(r'^$', views.PostListView.as_view(), name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.PostDetailsView.as_view(), name='post_detail'),
+    url(r'^post/new/$', views.PostNewView.as_view(), name='post_new'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.PostEditView.as_view(), name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,29 @@
+from django import views
 from django.shortcuts import render
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import TemplateView
+
 from .models import Post
 from .forms import PostForm
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts':posts})
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+class PostListView(TemplateView):
+    template_name = 'blog/post_list.html'
+    def get_context_data(self):
+        return {'posts': Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')}
 
-def post_new(request):
-    if request.method == "POST":
+
+class PostDetailsView(views.View):
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        return render(request, 'blog/post_detail.html', {'post': post})
+
+
+class PostNewView(views.View):
+    template_name = 'blog/post_edit.html'
+
+    def post(self, request):
        form = PostForm(request.POST)
        if form.is_valid():
            post = form.save(commit=False)
@@ -21,13 +31,18 @@ def post_new(request):
            post.published_date = timezone.now()
            post.save()
            return redirect('post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+       else:
+           return render(request, self.template_name, {'form': form})
 
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
+    def get(self, request):
+        return render(request, self.template_name, {'form': PostForm()})
+
+
+class PostEditView(views.View):
+    template_name = 'blog/post_edit.html'
+
+    def post(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
@@ -35,6 +50,11 @@ def post_edit(request, pk):
             post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
-    else:
+        else:
+            return render(request, self.template_name, {'form': form})
+
+    def get(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
         form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+        return render(request, self.template_name, {'form': form})
+
